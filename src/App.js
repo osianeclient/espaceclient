9b85404e874c5018190b75e3cdced159f9d4ed3d
Destino Dello <destinodello@gmail.com>
2021-07-02 18:43:09 +0000

    fixed private route error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import AdminLayout from "layouts/Admin.js";
 import AuthLayout from "layouts/Auth.js";
-import { AuthProvider } from "./contexts/AuthContext"
+import { AuthProvider } from "./contexts/AuthContext";
+import PrivateRoute from "components/PrivateRoute/PrivateRoute";
 import { BrowserRouter as Router , Route, Switch, Redirect } from "react-router-dom";
 
 function App () {
@@ -9,9 +10,9 @@ function App () {
         <Router>
             <AuthProvider>
                 <Switch>
-                    <Route path="/admin" render={props => <AdminLayout {...props} />} />
+                    <PrivateRoute exact path="/admin" render={props => <AdminLayout {...props} />} />
                     <Route path="/auth" render={props => <AuthLayout {...props} />} />
-                    <Redirect from="/" to="/admin/index" />
+                    <Redirect from="/" to="/admin" />
                 </Switch>
             </AuthProvider>
         </Router>
