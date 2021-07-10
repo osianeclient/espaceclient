f3c18c43c2c09348107f59904508ed79cad98ec6
Destino Dello <destinodello@gmail.com>
2021-07-10 01:45:03 +0000

    added apollo client to app

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import AuthLayout from "layouts/Auth.js";
 import { AuthProvider } from "./contexts/AuthContext";
 import PrivateRoute from "components/PrivateRoute/PrivateRoute";
 import { BrowserRouter as Router , Route, Switch, Redirect } from "react-router-dom";
+import { ApolloProvider } from '@apollo/client';
+import { apolloClient } from './apollo';
 
 function App () {
     return (
         <Router>
             <AuthProvider>
                 <Switch>
-                    <PrivateRoute path="/admin" component={AdminLayout} />
+                    <ApolloProvider client={apolloClient}>
+                        <PrivateRoute path="/admin" component={AdminLayout} />
+                    </ApolloProvider>
                     <Route path="/auth" render={props => <AuthLayout {...props} />} />
                     <Redirect from="/" to="/auth" />
                 </Switch>
